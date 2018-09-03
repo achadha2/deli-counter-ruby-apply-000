4a78c0b354e8962f6c1c5ed756fd40dbcb360369
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-03 14:31:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,11 @@
 katz_deli = []
 
 def line(deli = "empty.")
+  if
   #order = "The line is currently:"
-  #p "#{order}" + 
-  deli.each_with_index{|name, index| puts " #{index+1}. #{name}"}
+  p "#{order}" + deli.each_with_index{|name, index| puts " #{index+1}. #{name}"}
+else
+end
 end
 
 #def take_a_number()
