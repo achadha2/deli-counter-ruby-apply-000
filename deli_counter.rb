a9ba6f2aa56db8d5c3fb4717a8db6b1207217e87
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-03 14:53:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ def line(array)
     if array.empty?
       puts "The line is currently empty."
     else
-      q = []
-      array.each.with_index(1) do |index, person| 
-      q.push("#{person}. #{index}")
+      #q = []
+      #array.each.with_index(1) do |index, person| 
+     # q.push("#{person}. #{index}")
     end
-      puts "The line is currently: " + q.join(" ") 
+      puts "The line is currently:" 
+           array.each_with_index {|name, index| puts " #{index+1}. #{name}"
   end
 end
 
