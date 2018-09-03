3641e1f4e684ece688a65b1697b7184276ebf5e5
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-03 14:20:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 katz_deli = []
 
-def line(name="The line is currently empty.")
-  
+def line(line="empty.")
+  order = "The line is currently:"
+  line.each_with_index(|J)
 end
 
 def take_a_number()
