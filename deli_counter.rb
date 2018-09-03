e877802e3473c3a4598f9b01f1bca3d49a6079bd
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-03 14:54:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-katz_deli = []
+katz_deli = ["james"]
 
 def line(array)
     if array.empty?
@@ -8,7 +8,7 @@ def line(array)
       #array.each.with_index(1) do |index, person| 
      # q.push("#{person}. #{index}")
       puts "The line is currently:" 
-           array.each_with_index {|name, index| puts " #{index+1}. #{name}"
+      array.each_with_index {|name, index| puts " #{index+1}. #{name}"
   end
 end
 
